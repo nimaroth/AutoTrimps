bef00f4576d3f34ea36df5e3d5abc96be908139b
glieutard <43911638+glieutard@users.noreply.github.com>
2020-01-15 00:50:39 +0100

    lessen speed resources

--- modules/gather.js
+++ modules/gather.js
@@ -44,7 +44,8 @@ function manualLabor2() {
             var portUpgrade = game.portal[portItem];
             heliumSpent += portUpgrade.heliumSpent;
         }
-        game.resources.helium.respecMax = 100000000;
+        game.global.heliumLeftover = game.global.totalHeliumEarned - heliumSpent;
+        game.resources.helium.respecMax = game.global.totalHeliumEarned - heliumSpent;
         // game.heliumLeftover = game.totalHeliumEarned - heliumSpent;
 
         // game.resources.helium.owned = game.resources.helium.owned * 2 + 1000;
